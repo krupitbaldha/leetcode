0ff342ef6284e6e9aebf33dc283918042930fc9b
krupitbaldha <krupitbaldha12@gmail.com>
2026-07-14 03:02:49 +0530

    Time: 0 ms (100%), Space: 16 MB (76.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-
-        if(i == 0 && j == 0)
-            return grid[0][0];
-
-        if(i < 0 || j < 0)
-            return INT_MAX;
-
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        int up = f(i-1, j, grid, dp);
-        int left = f(i, j-1, grid, dp);
-
-        if(i>0) up =  grid[i][j] + up;
-        if(j>0)  left =  grid[i][j] + left;
-
-        return dp[i][j] = min(up, left);
-    }
+   
 
     int minPathSum(vector<vector<int>>& grid) {
 
         int n = grid.size();
         int m = grid[0].size();
+  vector<int>prev(m,0);
+  for(int i =0;i<n;i++){
+    vector<int>curri(m,0);
+    for(int j=0;j<m;j++){
+        if(i==0&&j==0)curri[i] = grid[i][j];
+        else{
+            int up = grid[i][j];
+            if(i>0) up+=prev[j];
+            else up+=1e9;
+            int left = grid[i][j];
+            if(j>0) left += curri[j-1];
+         else left +=1e9;
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-
-        return f(n-1, m-1, grid, dp);
+         curri[j] = min(up,left);
+        }
+    }
+    prev = curri;
+  }
+        return prev[m-1];
     }
 };
