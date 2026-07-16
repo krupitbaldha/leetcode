9f16cebf0210906e1f97c3ecb8cde856feef2a3f
krupitbaldha <krupitbaldha12@gmail.com>
2026-07-16 17:45:52 +0530

    Time: 31 ms (59.22%), Space: 22.1 MB (50.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int target) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(target+1, 1e9));
+        vector<int>prev(target+1,0),curr(target+1,0);
 
         // Base case: using only coins[0]
         for(int t = 0; t <= target; t++) {
-            if(t % coins[0] == 0) dp[0][t] = t / coins[0];
+            if(t % coins[0] == 0) prev[t] = t / coins[0];
+            else prev[t] = 1e9;
         }
 
         // Fill DP table
         for(int idx = 1; idx < n; idx++) {
             for(int tar = 0; tar <= target; tar++) {
-                int ntake = dp[idx-1][tar]; // not taking current coin
+                int ntake =prev[tar]; // not taking current coin
                 int take = 1e9;
-                if(coins[idx] <= tar) take = 1 + dp[idx][tar - coins[idx]];
-                dp[idx][tar] = min(take, ntake);
+                if(coins[idx] <= tar) take = 1 + curr[tar - coins[idx]];
+                curr[tar] = min(take, ntake);
             }
+           prev= curr; 
         }
 
-        return (dp[n-1][target] >= 1e9) ? -1 : dp[n-1][target];
+        return (prev[target] >= 1e9) ? -1 : prev[target];
     }
 };
