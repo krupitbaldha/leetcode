62d739103266be35d83bbbd1576e1c133f15d476
krupitbaldha <krupitbaldha12@gmail.com>
2026-07-18 18:01:15 +0530

    Time: 5 ms (72.99%), Space: 13.2 MB (42.08%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-int f(int i,int j,string s, string t,  vector<vector<int>>&dp){
-    if(i<0)return j+1;
-    if(j<0)return i+1;
-    if(dp[i][j]!=-1)return dp[i][j];
-    if(s[i]==t[j]) return dp[i][j] = f(i-1,j-1,s,t,dp);
-    return dp[i][j] = 1+ min(f(i-1,j,s,t,dp),min(f(i,j-1,s,t,dp),f(i-1,j-1,s,t,dp)));
-}
+
     int minDistance(string s, string t) {
         int n = s.length();
         int m = t.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(n-1,m-1,s,t,dp);
+        for(int j=0;j<=m;j++) dp[0][j] = j;
+        for(int i =0;i<=n;i++)dp[i][0] =i;
+
+        for(int i= 1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+                if(s[i-1]==t[j-1]) dp[i][j] = dp[i-1][j-1];
+    else dp[i][j] = 1+ min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
