4603ee0b57718a167799bd27191cd2306b1ea899
krupitbaldha <krupitbaldha12@gmail.com>
2026-07-19 12:50:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.23%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev = 1;
+        int prev2 = 1;
+       
+        for(int i =2;i<=n;i++){
+          int  ans = prev+prev2;
+         prev2 = prev;
+         prev = ans;
+        }
+        return prev;
+    }
+};
