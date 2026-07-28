a54a2589715130e2417486d62233c2f9c35d9be3
krupitbaldha <krupitbaldha12@gmail.com>
2026-07-28 23:20:09 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.84%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -7,13 +7,13 @@ public:
         int r = n-1;
         while(l<=r){
             int mid = (l+r)/2;
-            if(nums[mid]>=target){
-                 ans = mid;
-                r = mid-1;
+            if(nums[mid]<target){
+                 l = mid+1;
             }
             else{
-                l = mid+1;
-            
+                ans = mid;
+                r = mid-1;
+
             }
         }
         return  ans;
