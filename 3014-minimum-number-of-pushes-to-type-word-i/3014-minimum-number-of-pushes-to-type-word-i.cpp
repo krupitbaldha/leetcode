770da6021ae4dbbd642215b3d3bf2e2abc715b1a
krupitbaldha <krupitbaldha12@gmail.com>
2026-07-30 12:54:51 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int len = word.length();
+
+        if (len <= 8)
+            return len;
+
+        if (len <= 16)
+            return 8 + (len - 8) * 2;
+
+        if (len <= 24)
+            return 8 + 16 + (len - 16) * 3;
+
+        return 8 + 16 + 24 + (len - 24) * 4;
+    }
+};
